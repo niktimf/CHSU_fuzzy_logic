0bca2d70d200439cecd7c62b5cf2a0b141eaacb3
Nikita <98824238+niktimf@users.noreply.github.com>
2022-11-22 23:43:07 +0300

    Add files via upload

---
+++ Main.scala
@@ -0,0 +1,20 @@
+object Main:
+    def main(args: Array[String]) =
+        var initial_matrix = List(List(1, 2, 3, 4),
+                      List(1, 2, 3, 4),
+                      List(2, 4, 8, 9),
+                      List(2, 5, 3, 6))
+
+        def divMatrixMinMax(matrix1: List[List], matrix2: List[List]): matrix = List[Int]
+
+
+
+
+
+
+
+
+
+
+
+
