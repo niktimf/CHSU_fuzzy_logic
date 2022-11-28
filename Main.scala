fb79f5a5ebc165e72709c6e5217ba02b8e4844cf
Nikita <98824238+niktimf@users.noreply.github.com>
2022-11-28 20:07:09 +0300

    Add files via upload

--- Main.scala
+++ Main.scala
@@ -1,14 +1,17 @@
+//import pprint.PPrinter.BlackWhite.pprintln
+
 object Main:
-    def main(args: Array[String]) =
-        var initial_matrix = Array(Array(1, 2, 3, 4),
-                                   Array(1, 2, 3, 4),
-                                   Array(2, 4, 8, 9),
-                                   Array(2, 5, 3, 6))
+    def main(args: Array[String]): Unit =
+        def multMatrixMinMax(matrix1: Seq[Seq[Double]], matrix2: Seq[Seq[Double]]): Seq[Seq[Double]] =
+            for m2Row <- matrix2 yield for m1Col <- matrix1.transpose yield m1Col.lazyZip(m2Row).map(_ min _).max
 
-        def multMatrixMinMax(matrix1: Array[Array[Int]], matrix2: List[Array[Int]]): matrix = List[Int]
-
-
-
+        val matrix = Vector[Double](1, 2, 3, 5, 1,
+                                           4, 5, 6, 3, 3,
+                                           7, 8, 9, 9, 2,
+                                           4, 2, 6, 1, 4,
+                                           4, 2, 6, 1, 9
+        ).grouped(5).to(Vector)
+        println(multMatrixMinMax(matrix, matrix))
 
 
 
